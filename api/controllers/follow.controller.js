cb8f3b6aec53212a6ff26320d9d26cd518707fdf
Mathieu <spiie.pro@souflis.fr>
2024-09-02 14:52:49 +0200

    follow request missing await

--- api/controllers/follow.controller.js
+++ api/controllers/follow.controller.js
@@ -36,24 +36,31 @@ class FollowController {
     }
   };
 
-  follow = (req, res) => {
+  follow = async (req, res) => {
     try {
       const baseUrl = req.baseUrl.split("/");
       const user_id = baseUrl[baseUrl.length - 1];
       const { targeted_id } = req.body;
+      if (!user_id || !targeted_id)
+        return res.status(400).send("Missing arguments").end();
       if (user_id !== req.user.id || user_id === targeted_id) {
         return res.status(403).send("Forbidden");
       }
+      if (user_id === targeted_id)
+        return res.status(400).send("Can't follow yourself").end();
 
-      if (!UsersPrismaController.findUserById(targeted_id)) {
+      if (!(await UsersPrismaController.findUserById(targeted_id))) {
         return res.status(404).send("User not found");
       }
 
-      if (FollowPrismaController.isFollowing(user_id, targeted_id) === true) {
+      if (
+        (await FollowPrismaController.isFollowing(user_id, targeted_id)) ===
+        true
+      ) {
         return res.status(409).send("User allready followed");
       }
 
-      FollowPrismaController.follow(user_id, targeted_id);
+      await FollowPrismaController.follow(req.user.id, targeted_id);
 
       return res.status(201).send("User followed");
     } catch (error) {
@@ -62,7 +69,7 @@ class FollowController {
     }
   };
 
-  unfollow = (req, res) => {
+  unfollow = async (req, res) => {
     try {
       const baseUrl = req.baseUrl.split("/");
       const user_id = baseUrl[baseUrl.length - 1];
@@ -72,17 +79,18 @@ class FollowController {
         return res.status(403).send("Forbidden");
       }
 
-      if (!UsersPrismaController.findUserById(target_user_id)) {
+      if (!(await UsersPrismaController.findUserById(target_user_id))) {
         return res.status(404).send("User not found");
       }
 
       if (
-        FollowPrismaController.isFollowing(user_id, target_user_id) === false
+        (await FollowPrismaController.isFollowing(user_id, target_user_id)) ===
+        false
       ) {
         return res.status(404).send("Follow not found");
       }
 
-      FollowPrismaController.unfollow(user_id, target_user_id);
+      await FollowPrismaController.unfollow(user_id, target_user_id);
 
       return res.status(200).send("User unfollowed");
     } catch (error) {
