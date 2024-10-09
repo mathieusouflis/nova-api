aa81a339ce8aabbaf2b2b78df7ddce644992a3b9
Mathieu <spiie.pro@souflis.fr>
2024-10-09 20:53:35 +0200

    try to setup https

--- index.js
+++ index.js
@@ -1,8 +1,16 @@
 import { port } from "./constants/dev.js";
 import "./constants/dev.js";
 import app from "./config/express.config.js";
+import https from "https";
 
-app.listen(port, (err) => {
+const credentials = {
+  key: process.env.SERVER_KEY,
+  cert: process.env.SERVER_CERT,
+};
+
+const httpsServer = https.createServer(credentials, app);
+
+httpsServer.listen(port, (err) => {
   if (err) {
     console.log(err);
   }
