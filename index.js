1578a5444d518da0cf9af92449666630efba699a
Mathieu <spiie.pro@souflis.fr>
2024-10-09 21:06:21 +0200

    Https try 3

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
 import { port } from "./constants/dev.js";
 import "./constants/dev.js";
+import fs from "fs";
 import app from "./config/express.config.js";
 import https from "https";
+import constants from "constants";
 
 const credentials = {
-  key: process.env.SERVER_KEY,
-  cert: process.env.SERVER_CERT,
+  key: fs.readFileSync("server.key", "utf8"),
+  cert: fs.readFileSync("server.cert", "utf8"),
+  secureOptions:
+    constants.SSL_OP_NO_SSLv3 |
+    constants.SSL_OP_NO_TLSv1 |
+    constants.SSL_OP_NO_TLSv1_1,
 };
 
 const httpsServer = https.createServer(credentials, app);
