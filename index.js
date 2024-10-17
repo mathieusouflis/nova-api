f455f898fc10d438325c77d63d9ff035d659f99a
Mathieu <spiie.pro@souflis.fr>
2024-10-17 21:11:42 +0200

    cert remove + try to host on railway

--- index.js
+++ index.js
@@ -1,24 +1,10 @@
 import { port } from "./constants/dev.js";
 import "./constants/dev.js";
-import fs from "fs";
 import app from "./config/express.config.js";
-import https from "https";
-import constants from "constants";
 
-const credentials = {
-  key: fs.readFileSync("server.key", "utf8"),
-  cert: fs.readFileSync("server.cert", "utf8"),
-  secureOptions:
-    constants.SSL_OP_NO_SSLv3 |
-    constants.SSL_OP_NO_TLSv1 |
-    constants.SSL_OP_NO_TLSv1_1,
-};
-
-const httpsServer = https.createServer(credentials, app);
-
-httpsServer.listen(port, (err) => {
+app.listen(port, "0.0.0.0", (err) => {
   if (err) {
     console.log(err);
   }
-  console.log(`App started : http://localhost:${port}/api`);
+  console.log("I'm online at http://localhost:" + port);
 });
